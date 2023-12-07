d2301413253814285f575ddb12a3e3ac45f3598e
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2023-12-07 12:30:48 -0500

    Initial commit =)))

---
+++ src/Hobbies.java
@@ -0,0 +1,21 @@
+public class Hobbies {
+    String nombre;
+    double tiempo;
+    public Hobbies(String nombre, double tiempo) {
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getTiempo() {
+        return tiempo;
+    }
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+}
